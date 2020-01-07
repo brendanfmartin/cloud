2983f5d2d117b594b8cdc0774938dac94f8740aa
brendanfmartin <brendanfmartin@gmail.com>
2020-01-06 22:43:14 -0500

    Sample map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,26 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  private readonly access_token = 'pk.eyJ1IjoiYnJlbmRhbmZtYXJ0aW4iLCJhIjoiY2s1M2JnbTV5MDZ0djNrcGh0cXN0d2Y2bSJ9.4JYMZDKVqdJS0dJA0USyJw';
+  private readonly url = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/1/1/0?access_token=${this.access_token}`;
+  private readonly attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+
   thoughts: object[] = [];
+
+  L: any = window['L'];
 
   ngOnInit() {
     this.getThougths();
+
+    const mymap = this.L.map('mapid').setView([51.505, -0.09], 13);
+    this.L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: this.attribution,
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      accessToken: this.access_token
+    }).addTo(mymap);
+
+    const marker = this.L.marker([51.5, -0.09]).addTo(mymap);
   }
 
   newThought(): void {
