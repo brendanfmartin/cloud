161f16a8b63a4060f279fbf99bd25755c6574abb
brendanfmartin <brendanfmartin@gmail.com>
2020-01-06 21:36:32 -0500

    Thought collector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cloud';
+export class AppComponent implements OnInit {
+
+  thoughts: object[] = [];
+
+  ngOnInit() {
+    this.getThougths();
+  }
+
+  newThought(): void {
+    this.getThougths();
+  }
+
+  private getThougths(): void {
+    console.log(this.thoughts);
+    const localThoughts = localStorage.getItem('thoughts');
+    try {
+      this.thoughts = JSON.parse(localThoughts);
+      console.log(this.thoughts)
+    } catch (e) {
+      console.error(e);
+      this.thoughts = [];
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BubbleComponent } from './bubble/bubble.component';
+import { NewThoughtComponent } from './new-thought/new-thought.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatSliderModule } from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BubbleComponent,
+    NewThoughtComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // material
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
