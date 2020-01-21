0464fc67d6d784ae780e281ffc0e53525ecfd9bf
brendanfmartin <brendanfmartin@gmail.com>
2020-01-20 22:20:18 -0500

    Working rest endpoints

--- node-app/src/app.js
+++ node-app/src/app.js
@@ -1,8 +1,12 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const uuidv4 = require('uuid/v4');
+const bodyParser = require('body-parser');
 
-const thoughts = {};
+app.use(bodyParser.json());
+
+let thoughts = {};
 
 io.on('connection', socket => {
   let previousId;
@@ -34,6 +38,33 @@ io.on('connection', socket => {
   console.log(`Socket ${socket.id} has connected`);
 });
 
-http.listen(4444, () => {
-  console.log('Listening on port 4444');
+const port = process.env.PORT || 3000;
+
+http.listen(port, () => {
+  console.log('Listening on port', port);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!').end();
+});
+
+app.get('/thoughts', (req, res) => {
+  res.send(thoughts).end();
+});
+
+app.post('/post-test', (req, res) => {
+  console.log('Got body:', req.body);
+  res.sendStatus(200);
+});
+
+app.post('/thought', (req, res) => {
+  console.log(req);
+  const thoughtId = uuidv4();
+  thoughts[thoughtId] = req.body;
+  res.json({message: 'accepted', thoughts}).end();
+});
+
+app.delete('/thoughts', (req, res) => {
+  thoughts = {};
+  res.json({message: 'deleted', thoughts}).end();
 });
